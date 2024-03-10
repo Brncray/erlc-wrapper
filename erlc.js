6ec06d0954c043e90896cc1cb3e0174a61a832d5
Brncray <57870234+Brncray@users.noreply.github.com>
2024-03-10 13:05:39 -0400

    .

---
+++ erlc.js
@@ -0,0 +1,36 @@
+let key = "";
+import fetch from 'node-fetch';
+const url = "https://api.policeroleplay.community/v1"
+
+
+export const erlc = {
+
+
+  /**
+   * Set the key for the erlc module.
+   * @param {string} newKey - The new key to set.
+   */
+  setKey(newKey) {
+    key = newKey;
+  },
+
+  /**
+   * Get the currently set key for the erlc module.
+   * @returns {string} The currently set key.
+   */
+  getKey() {
+    return key;
+  },
+
+  /**
+   * Fetch the server status from the ERLC API.
+   * @returns {Promise<object>} The server status.
+   */
+  async serverStatus() {
+    const response = await fetch(url + "/server") 
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch server status. Maybe check your API key?");
+    }
+    return response.json();
+  }
+};
---
+++ index.js
@@ -0,0 +1,1 @@
+export { erlc } from "./erlc.js";
